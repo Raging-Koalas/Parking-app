263ccd5821f5e192647a5fdba00ee62e793d424a
Rownok <rownok2001@gmail.com>
2022-08-23 23:15:58 +0600

    Rownok

--- components/Chat.js
+++ components/Chat.js
@@ -37,7 +37,7 @@ const Chat = () => {
           <div className="fixed inset-0 bg-black bg-opacity-50" />
         </Transition.Child>
 
-        <div className="fixed inset-0 overflow-y-auto">
+        <div className="fixed inset-0 overflow-y-auto ">
           <div className="flex min-h-full items-end justify-end p-4  text-center">
             <Transition.Child
               as={Fragment}
@@ -48,27 +48,31 @@ const Chat = () => {
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl dark:bg-black bg-white p-6 text-left align-middle shadow-xl transition-all">
                 <Dialog.Title
                   as="h3"
                   className="text-lg font-medium leading-6 text-gray-900"
                 >
-                  Payment successful
+                 Pranto Boss
                 </Dialog.Title>
-                <div className="mt-2">
-                  <p className="text-sm text-gray-500">
-                    Your payment has been successfully submitted. We’ve sent
-                    you an email with all of the details of your order.
-                  </p>
-                </div>
-
-                <div className="mt-4">
+                <img onClick={closeModal} className='w-7 rounded-2xl cursor-pointer p-2 absolute top-4 right-4 bg-gray-300 dark:bg-gray-500' src="/Assets/cross.svg" alt="" />
+              
+              <p className='pl-4 bg-gray-100 w-56 py-2 my-2 rounded-lg text-black items-center'>Hi How are you !</p>
+              
+              <p className='text-black font-semibold flex justify-end items-center'>Rownok Mahbub</p>
+              
+              <div className='flex justify-end'>
+              <p className='pr-4 bg-gray-100 w-56 py-2 my-2 rounded-lg text-black flex justify-end items-center'>Hi How are you !</p>
+              </div>
+             
+                <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mt-4" placeholder="Type Your Text here..." required/>
+                <div className="mt-4 flex justify-end">
                   <button
                     type="button"
                     className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
                     onClick={closeModal}
                   >
-                    Got it, thanks!
+                    Send
                   </button>
                 </div>
               </Dialog.Panel>
--- components/Navbar.js
+++ components/Navbar.js
@@ -5,7 +5,7 @@ const Nav = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
-    <nav className="bg-gray-800">
+    <nav className="dark:bg-gray-800 bg-slate-100 ">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
         <div className="flex-shrink-0">
@@ -17,37 +17,38 @@ const Nav = () => {
               <div className="ml-10 flex items-baseline space-x-4">
                 <a
                   href="#"
-                  className=" hover:bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className=" hover:bg-gray-700 text-black dark:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
-                  Dashboard
+                  Home
                 </a>
 
                 <a
                   href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className=" hover:bg-gray-700 hover:text-black dark:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
-                  Team
+                  About
                 </a>
 
                 <a
                   href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className=" hover:bg-gray-700 hover:text-black dark:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
-                  Projects
+                  Service
                 </a>
 
                 <a
                   href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className=" hover:bg-gray-700 hover:text-black dark:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
-                  Calendar
+                  Pricing
+                
                 </a>
 
                 <a
                   href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className=" hover:bg-gray-700 hover:text-black dark:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
-                  Reports
+                    Contact Us
                 </a>
               </div>
             </div>
@@ -56,7 +57,7 @@ const Nav = () => {
             <button
               onClick={() => setIsOpen(!isOpen)}
               type="button"
-              className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+              className="dark:bg-gray-900 text-black inline-flex items-center justify-center p-2 rounded-md dark:text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
               aria-controls="mobile-menu"
               aria-expanded="false"
             >
@@ -120,28 +121,28 @@ const Nav = () => {
 
               <a
                 href="#"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                className=" hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
               >
                 Team
               </a>
 
               <a
                 href="#"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                className=" hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
               >
                 Projects
               </a>
 
               <a
                 href="#"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                className=" hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
               >
                 Calendar
               </a>
 
               <a
                 href="#"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                className=" hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
               >
                 Reports
               </a>
